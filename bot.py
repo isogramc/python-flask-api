b99feaa8e5f6fb7c529e9536280c8d7ad017aa14
Theresa <theresa@isogramconsulting.com>
2020-01-23 10:12:31 +0200

    Brackets

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from flask import Flask
 import requests
 import json
 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,10 +12,11 @@ def bot():
     if r.status_code == 200:
         data = r.json()
         recipe = data["drinks"]
-        name = item['strDrink'] for item in recipe
+        name = [item["strDrink"] for item in recipe]
     else:
         name = "I could not retrieve your recipe"
     return str(name)
+
 
 if __name__ == "__main__":
     app.run()
